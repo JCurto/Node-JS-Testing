b97cf8b0f912176ee43791e7bb7cf929fa1ebe70
JCurto <jeffery.curto@gmail.com>
2015-06-09 23:01:44 -0400

    Revert "test commit"
    This reverts commit c2b468c778131ddb197f68343476936a7a9c1107.

--- server.2.js
+++
@@ -1,9 +0,0 @@
-var express = require('express');
-var app = express();
-
-app.use(express.static('index'));
-app.use(express.static('TestPage1'));
-
-app.get('/', function (req, res) { });
-
-var server = app.listen(8888, function () { });
